1ba45043d99ab7686de1081c18641915cf884592
Teri11 <augustineteri157@gmail.com>
2022-08-03 15:20:39 +0300

    added reactive value

--- src/Home.js
+++ src/Home.js
@@ -4,17 +4,19 @@ import { useState } from "react";
 const Home = () => {
  
  let [name, setName] = useState('Teri');
+ let [age, setAge] = useState(25);
 
 
   let handClick = () => {
     setName('Augustine')
+    setAge(30);
 
   }
 
   return ( 
     <div className="home">
       <h2>Homepage</h2>
-      <p>{name}</p>
+      <p>{name} is {age} years old.</p>
       <button onClick={handClick}>Click Me</button>
      </div>
    );

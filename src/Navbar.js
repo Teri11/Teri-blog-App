67413e6ec98b8117af1d54097e44f928b58f7c67
Teri11 <augustineteri157@gmail.com>
2022-08-03 08:47:14 +0300

    added sfc snippet

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return ( 
+    <nav className="navbar">
+      <h1>The Teri Blog App</h1>
+      <div className="links">
+        <a href="/"></a>
+
+      </div>
+    </nav>
+   );
+}
+ 
+export default Navbar;

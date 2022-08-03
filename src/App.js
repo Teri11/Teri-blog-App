3569b01715baff56e00ce2e4a0aa3b6286f0da90
Teri11 <augustineteri157@gmail.com>
2022-08-03 08:42:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-<div className="content">
-  <h1>Augustine React First App</h1>
-  <h3>App component</h3>
-</div>
+    <div className="content">
+       <h3>App component</h3>
+       
+      </div>
     </div>
   );
 }
